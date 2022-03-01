fd1ec8c92ba7a70dc5b3b39e5ac6a5267ce22fa5
Dale Floer <dalefloer@gmail.com>
2022-03-01 07:16:54 -0800

    Added a bunch of geo stuff, not all working.
    Major things are projections and smarter subclasses of stuff.
    
    The projection is intended to project between XY and latlon
    points/bboxes, as some maps take one over the other. The projecting
    doesn't _quite_ seem to work yet.

--- static_maps/geo.py
+++ static_maps/geo.py
@@ -1,49 +1,184 @@
 from collections import namedtuple
 from typing import Any, Iterable, Tuple, Union
 
-from attrs import define, field
+from attrs import define, field, frozen
 from attrs.validators import instance_of
 from loguru import logger
+import mercantile
+import math
 
-Point = namedtuple("Point", ("x", "y"))
-Pixel = namedtuple("Pixel", ("x", "y"))
+# This is intended as a convenient shorthand for now, but it should be better handled.
+valid_float_int_to_float = field(converter=float, validator=instance_of((float, int)))
+
+LatLonExtents = namedtuple("LatLonExtents", ("lat, lon, rv"))(85.051129, 180, 100)
+xyExtents = namedtuple("xyExtents", ("x, y, rv"))(
+    20037508.342789244, 20037508.342789244, 100
+)
+
+
+def clamp(a: Union[float, int], v: Union[float, int]) -> Union[float, int]:
+    """Clamps a to [-v, +v]"""
+    assert v >= 0, f"Clamp value must be positive, not {v}."
+    return min(max(a, -v), v)
+
+
+def lat_converter(lat: Union[int, float]) -> float:
+    """Convenience function for clamp_float_round fot lat"""
+    lle = LatLonExtents
+    return clamp_float_round(lat, lle.lat, lle.rv)
+
+
+def lon_converter(lon: Union[int, float]) -> float:
+    """Convenience function for clamp_float_round fot lon"""
+    lle = LatLonExtents
+    return clamp_float_round(lon, lle.lon, lle.rv)
+
+
+def x_xy_converter(x_xy: Union[int, float]) -> float:
+    """Convenience function for clamp_float_round fot x"""
+    xye = xyExtents
+    return clamp_float_round(x_xy, xye.x, xye.rv)
+
+
+def y_xy_converter(y_xy: Union[int, float]) -> float:
+    """Convenience function for clamp_float_round fot y"""
+    xye = xyExtents
+    return clamp_float_round(y_xy, xye.y, xye.rv)
+
+
+def clamp_float_round(
+    v: Union[int, float], ex: Union[int, float], rv: int
+) -> Union[float, Any]:
+    """
+    Clamps the value v to the extent given as ex, and rounds to rv.
+    Args:
+        v (Union[int, float]): Value to clamp.
+        ex (Union[int, float]): EXtent to clamp the value to, inclusive.
+        rv (Union[int, float]): Roundint places Value.
+    Returns:
+        Union[float, Any]: either the rounded, clamped float, or a passthrough to the underlying validator.
+    """
+    try:
+        cc = clamp(v, ex)
+        r = float(round(cc, rv))
+        return r
+    except Exception:
+        return v
+
+
+lat_field = field(converter=lat_converter, validator=instance_of(float))
+lon_field = field(converter=lon_converter, validator=instance_of(float))
+
+x_field = field(converter=x_xy_converter, validator=instance_of(float))
+y_field = field(converter=y_xy_converter, validator=instance_of(float))
+
+
+@frozen(slots=True, weakref_slot=False)
+class BasePoint:
+    """
+    Baseclass with no attributes. Probably don't use this.
+    """
+
+    def __iter__(self) -> Iterable[Tuple[float, float]]:
+        """
+        __slots__ contains all the attributes of the class. We only want to iterate over the data containing attrs.
+        For the child classes, this means removing the crs. For this class, the __weakref__.
+        __match_args__ would work better, but it's Python >=3.10 feature with the latest attrs.
+        """
+        x = [
+            getattr(self, s)
+            for s in list(self.__slots__)
+            if s not in ["crs", "__weakref__"]
+        ]
+        return iter(x)
+
+    def __len__(self) -> int:
+        return len([x for x in self.__slots__ if x not in ["crs", "__weakref__"]])
+
+    def __getitem__(self, idx: int) -> Union[float, float]:
+        try:
+            return tuple(self)[idx]
+        except Exception as e:
+            msg = e.__str__().replace("tuple", type(self).__name__)
+            raise e.__class__(msg)
+
+
+@frozen(slots=True)
+class Point(BasePoint):
+    """x, y point with a CRS and bounds clamping."""
+
+    x: float = valid_float_int_to_float
+    y: float = valid_float_int_to_float
+
+
+# Alias for point.
+Pixel = Point
+
+
+@frozen(slots=True)
+class xyPoint(BasePoint):
+    """X, Y point with a CRS and bounds clamping."""
+
+    x: float = x_field
+    y: float = y_field
+    crs: str = field(default="EPSG:3857", init=False)
+
+
+@frozen(slots=True)
+class LatLon(BasePoint):
+    """lat, lon point with a CRS and bounds clamping."""
+
+    lat: float = lat_field
+    lon: float = lon_field
+    crs: str = field(default="EPSG:4326", init=False)
 
 
 @define
 class BBoxBase:
-    left: float = field(converter=float, validator=instance_of((float, int)))
-    top: float = field(converter=float, validator=instance_of((float, int)))
-    right: float = field(converter=float, validator=instance_of((float, int)))
-    bottom: float = field(converter=float, validator=instance_of((float, int)))
-    crs: str = field(default="EPSG:3857", validator=instance_of(str))
+    """
+    Base class for the bounding box. This could be used directly as a generic bounding box, if needed.
+    """
+
+    left: float = valid_float_int_to_float
+    top: float = valid_float_int_to_float
+    right: float = valid_float_int_to_float
+    bottom: float = valid_float_int_to_float
     point_type: namedtuple = field(default=Point, init=False, repr=False)
+    crs: str = field(default="", validator=instance_of(str))
 
     @property
-    def tl(self) -> int:
+    def tl(self) -> Point:
+        """Top Left corner point"""
         return self.point_type(self.left, self.top)
 
     @property
-    def br(self) -> int:
+    def br(self) -> Point:
+        """Bottom Right corner point"""
         return self.point_type(self.right, self.bottom)
 
     @property
-    def x_dim(self) -> int:
+    def x_dim(self) -> Union[float, int]:
+        """x (left-right) dimension"""
         return max(self.left, self.right) - min(self.left, self.right)
 
     @property
-    def y_dim(self) -> int:
+    def y_dim(self) -> Union[float, int]:
+        """y (top-bottom) dimension"""
         return max(self.top, self.bottom) - min(self.top, self.bottom)
 
     @property
-    def xy_dims(self) -> Tuple[int, int]:
+    def xy_dims(self) -> Tuple[Union[float, int], Union[float, int]]:
+        """x and y dimensions"""
         return self.x_dim, self.y_dim
 
     @property
     def area(self) -> int:
+        """Naive are calculation. crs setting would affect this."""
         return self.x_dim * self.y_dim
 
     @property
-    def center(self) -> Any:
+    def center(self) -> Point:
+        """Center of this bbox."""
         c_x = self.left + (self.right - self.left) / 2
         c_y = self.top + (self.bottom - self.top) / 2
         return self.point_type(c_x, c_y)
@@ -72,6 +207,7 @@ class BBox(BBoxBase):
     """
     This is a bit weird looking, but the goal is to be able to just drop arbitrary bad input on a BBox, and have it (try to) make something reasonable out of it.
     This means a mix of mandatory args and kwargs, and an optional kwarg.
+    ToDo: would be really nice to have the same conversion and validation on this as on the point classes.
     """
 
     def __init__(self, *args, **kwargs):
@@ -104,3 +240,107 @@ class BBox(BBoxBase):
                 return v
         err = f"{k} is not a supported alias."
         raise TypeError(err)
+
+
+# @frozen
+@define
+class LatLonBBox(BBox):
+    crs: str = field(default="EPSG:4326", init=False)
+    point_type: LatLon = field(default=LatLon, init=False, repr=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+@frozen
+class xyBBox(BBox):
+    crs: str = field(default="EPSG:3857", init=False)
+    point_type: xyPoint = field(default=xyPoint, init=False, repr=False)
+
+    def wms_str(self):
+        return f"{self.left},{self.bottom},{self.top},{self.right}"
+
+
+@define
+class Projector:
+    # Currently only supports 4326 and 3857.
+    out_crs: str
+
+    size_of_earth: int = field(default=6378137, init=False)  # WGS84
+    earth_circ: float = field(default=20037508.342789244, init=False)  # in meters
+    latlon_extents: namedtuple = field(default=LatLonExtents, init=False)
+    xy_extents: namedtuple = field(default=xyExtents, init=False)
+
+    def project(self, obj):
+        if obj.crs == self.out_crs:
+            return obj
+        if isinstance(obj, BBoxBase):
+            return self._project_bbox(obj)
+        else:
+            raise NotImplementedError
+
+    def project_bbox(self, bbox, in_crs):
+        """
+        Project the given bbox into our crs.
+        """
+        # degrees form
+        if "4326" in in_crs:
+            mt = mercantile.bounding_tile(*bbox)
+            l, b, r, t = mercantile.xy_bounds(*mt)
+            return xyBBox(left=l, top=t, right=r, bottom=b)
+        # XY form
+        if "3857" in in_crs:
+            mt = mercantile.bounding_tile(*bbox)
+            l, b, r, t = mercantile.xy_bounds(mt)
+            nl, nt = self._project_point(l, t)
+            nr, nb = self._project_point(r, b)
+            return LatLonBBox(left=nl, top=nt, right=nr, bottom=nb)
+
+    def project_point(self, pnt, in_crs):
+        """Protects the given point into our crs."""
+        # degrees form
+        if "4326" in in_crs:
+            return self.latlon_to_xy(pnt)
+        # XY form
+        if "3857" in in_crs:
+            return self.xy_to_latlon(pnt)
+
+    def latlon_to_xy(self, pnt: Point) -> xyPoint:
+        """Converts 4326 to 3857"""
+        lat, lon = pnt
+        # Unclear if these checks are even needed, but they seem like a good idea anyways.
+        lat_ext, lon_ext, rounv_v = self.latlon_extents
+        assert abs(lat) <= lat_ext, f"lat ({lat}) must be in [-{lat_ext}, {lat_ext}]."
+        assert abs(lon) <= lon_ext, f"lon ({lon}) must be in [-{lon_ext}, {lon_ext}]."
+
+        mx = lon * self.earth_circ / 180.0
+        y = math.log(math.tan((90 + lat) * math.pi / 360.0)) / (math.pi / 180.0)
+        my = y * self.earth_circ / 180.0
+        return xyPoint(round(mx, rounv_v), round(my, rounv_v))
+
+    def xy_to_latlon(self, pnt: Point) -> LatLon:
+        """Converts 3857 to 4326"""
+        mx, my = pnt
+        # Unclear if these checks are even needed, but they seem like a good idea anyways.
+        x_ext, y_ext, rounv_v = self.xy_extents
+        assert abs(mx) <= x_ext, f"x {mx} must be in [-{x_ext}, {x_ext}]."
+        assert abs(my) <= y_ext, f"y {my} must be in [-{-y_ext}, {y_ext}]."
+
+        lon = mx / self.earth_circ * 180.0
+        y = my / self.earth_circ * 180.0
+        x = math.atan(math.exp(y * math.pi / 180.0))
+        lat = 180 / math.pi * (2 * x - math.pi / 2.0)
+        return LatLon(round(lat, rounv_v), round(lon, rounv_v))
+
+
+def tid_to_xy_bbox(tid) -> xyBBox:
+    z, x, y = tid
+    print(tid)
+    w, s, e, n = mercantile.xy_bounds(z, x, y)
+    # ll_bb = LatLonBBox(n=n, s=s, w=w, e=e)
+    # print(ll_bb)
+    # p = Projector(out_crs="EPSG:3857")
+    # xy_bb = p.project_bbox(ll_bb, "EPSG:4326")
+    # print(xy_bb)
+    return xyBBox(w, n, e, s)
+    # return xy_bb
--- static_maps/test_static_maps/test_geo.py
+++ static_maps/test_static_maps/test_geo.py
@@ -1,13 +1,151 @@
+from ast import Index
 import os
 import sys
 
 import pytest
 from loguru import logger
+from collections import namedtuple
 
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
 sys.path.append(os.getcwd())
-from static_maps.geo import BBox, BBoxBase, Pixel, Point
+from static_maps.geo import (
+    BBox,
+    BBoxBase,
+    Pixel,
+    Point,
+    LatLon,
+    LatLonBBox,
+    xyBBox,
+    xyPoint,
+    Projector,
+    BasePoint,
+)
+import static_maps.geo as geo
+
+
+@pytest.mark.parametrize(
+    "a, k",
+    [
+        ((0, 0), {}),
+        ((), {"x": 0, "y": 0}),
+        ((0,), {"y": 0}),
+    ],
+)
+class TestPoint:
+    def test_create_point(self, a, k):
+        _ = Point(*a, **k)
+
+    def test_create_pixel(self, a, k):
+        _ = Pixel(*a, **k)
+
+    def test_create_latlon(self, a, k):
+        m = {"x": "lat", "y": "lon"}
+        k = {m[a]: v for a, v in k.items()}
+        _ = LatLon(*a, **k)
+
+
+class TestBasePoint:
+    def test_create_bp(self):
+        _ = BasePoint()
+
+    def test_len_bp(self):
+        bp = BasePoint()
+        assert len(bp) == 0
+
+    def test_iter_bp(self):
+        bp = BasePoint()
+        assert tuple(bp) == ()
+
+
+class TestLatLon:
+    max_lat, max_long, _ = geo.LatLonExtents
+
+    def test_create_llp(self):
+        llp = LatLon(0, 0)
+        assert llp
+
+    def test_llp_iter(self):
+        llp = LatLon(0, 0)
+        assert tuple(llp) == (0, 0)
+
+    def test_llp_len(self):
+        llp = LatLon(0, 0)
+        assert len(llp) == 2
+
+    def test_llp_getitem(self):
+        llp = LatLon(1, 2)
+        assert llp[0] == 1
+        assert llp[1] == 2
+
+    def test_llp_getitem_index_fail(self):
+        llp = LatLon(0, 0)
+        with pytest.raises(IndexError):
+            assert llp[2] == 0
+
+    def test_llp_getitem_type_fail(self):
+        llp = LatLon(0, 0)
+        with pytest.raises(TypeError):
+            assert llp["4"] == 4
+
+    @pytest.mark.parametrize(
+        "latlon, exp_val",
+        [
+            ((0, 0), (0, 0)),
+            ((10, 10), (10.0, 10.0)),
+            ((100, 200), (max_lat, max_long)),
+            ((-100, -200), (-max_lat, -max_long)),
+            ((-100, 200), (-max_lat, max_long)),
+            ((100, -200), (max_lat, -max_long)),
+        ],
+    )
+    def test_llp_conv_verif(self, latlon, exp_val):
+        llp = LatLon(*latlon)
+        assert tuple(llp) == exp_val
+        assert isinstance(llp.lat, float)
+        assert isinstance(llp.lon, float)
+
+    def test_llp_conv_verif_fail(self):
+        with pytest.raises(TypeError):
+            _ = LatLon("12.0", "14.0")
+
+
+class TestxyPoint:
+    max_x, max_y, _ = geo.xyExtents
+
+    def test_create_xyp(self):
+        xyp = xyPoint(0, 0)
+        assert xyp.x == 0 and xyp.y == 0
+
+    @pytest.mark.parametrize(
+        "x_y, exp_val",
+        [
+            ((0, 0), (0, 0)),
+            ((max_x, max_y), (max_x, max_y)),
+            ((-max_x, -max_y), (-max_x, -max_y)),
+            ((max_x * 2, max_y * 2), (max_x, max_y)),
+            ((max_x * 2, -max_y * 2), (max_x, -max_y)),
+            ((max_x, max_y * 2), (max_x, max_y)),
+            ((-max_x * 2, max_y), (-max_x, max_y)),
+        ],
+    )
+    def test_xyp_conv_verify(self, x_y, exp_val):
+        xyp = xyPoint(*x_y)
+        assert tuple(xyp) == exp_val
+        assert isinstance(xyp.x, float)
+        assert isinstance(xyp.y, float)
+
+    def test_xyp_conv_verif_fail(self):
+        with pytest.raises(TypeError):
+            _ = xyPoint("12.0", "14.0")
+
+    def test_xyp_iter(self):
+        xyp = xyPoint(0, 0)
+        assert tuple(xyp) == (0, 0)
+
+    def test_xyp_len(self):
+        xyp = xyPoint(0, 0)
+        assert len(xyp) == 2
 
 
 class TestBboxBase:
@@ -233,3 +371,109 @@ class TestBBox:
         res_bbox = BBox(1, 2, 3, 4, crs="x")
         aliases = res_bbox._aliases
         assert aliases
+
+
+class TestProjector:
+    # max_lat, max_lon, _ = geo.LatLonExtents
+    # max_x, max_y, _ = geo.xyExtents
+    @pytest.mark.parametrize(
+        "crs",
+        [
+            "EPSG:4326",
+            "EPSG:3857",
+        ],
+    )
+    def test_create(self, crs):
+        p = Projector(crs)
+        assert p.out_crs == crs
+
+    @pytest.mark.skip("WiP")
+    @pytest.mark.parametrize(
+        "in_bbox, in_crs, exp_bbox, exp_crs",
+        [
+            [(-45, -90, 45, 90), "EPSG:4326", (5, 6, 7, 8), "EPSG:3857"],
+        ],
+    )
+    def test_project_bbox(self, in_bbox, in_crs, exp_bbox, exp_crs):
+        p = Projector(in_crs)
+        bbox = LatLonBBox(*in_bbox)
+        res = p.project(bbox)
+        # assert res.crs == exp_crs
+        assert tuple(x for x in res) == exp_bbox
+
+    @pytest.mark.skip("Need to figure out a better way to compare floats.")
+    @pytest.mark.parametrize(
+        "latlon, xy",
+        [
+            (LatLon(0, 0), xyPoint(0, 0)),
+            (LatLon(45, 85.06), xyPoint(10018754.17139462, 5621521.48619207)),
+            (LatLon(-85.06, -180), xyPoint(-20037508.34278924, -242528680.94374272)),
+            (LatLon(85.06, 180), xyPoint(20037508.34278924, 242528680.94374272)),
+            (LatLon(56.47876683, 11.09030405), xyPoint(1234567, 7654321)),
+        ],
+    )
+    def test_latlon_and_xy_conversion(self, latlon, xy):
+        p = Projector(None)
+        res = p.latlon_to_xy(latlon)
+        print(res)
+        assert res[0] == pytest.approx(xy[0])
+        assert res[1] == pytest.approx(xy[1])
+        res2 = p.xy_to_latlon(xy)
+        print(res2)
+        assert res2[0] == pytest.approx(latlon[0])
+        assert res2[1] == pytest.approx(latlon[1])
+
+    @pytest.mark.parametrize(
+        "typ, pnt",
+        [
+            ("ll", (200, 0)),
+            ("ll", (0, 200)),
+            ("ll", (200, 200)),
+            ("xy", (20037510, 0)),
+            ("xy", (0, 242528681)),
+            ("xy", (20037510, 242528681)),
+        ],
+    )
+    def test_fail_latlon_and_xy_conversion(self, typ, pnt):
+        p = Projector(None)
+        if typ == "ll":
+            with pytest.raises(AssertionError):
+                print(p.latlon_to_xy(pnt))
+        else:
+            with pytest.raises(AssertionError):
+                print(p.xy_to_latlon(pnt))
+
+
+class TestGeoUtils:
+    @pytest.mark.parametrize(
+        "in_val, clamp_val,  exp_val, exc",
+        [
+            (100, 200, 100, None),
+            (100, 20, 20, None),
+            (-100.2, 20.02, -20.02, None),
+            (10, -10, -10, AssertionError),
+            (-10, -20, -10, AssertionError),
+        ],
+    )
+    def test_bp_clamp(self, in_val, clamp_val, exp_val, exc):
+        bp = BasePoint()
+        if exc:
+            with pytest.raises(exc):
+                r = geo.clamp(in_val, clamp_val)
+        else:
+            r = geo.clamp(in_val, clamp_val)
+            assert r == exp_val
+
+    @pytest.mark.parametrize(
+        "in_val, clamp_val,  exp_val, round_val",
+        [
+            (100, 200, 100.0, 8),
+            (100, 20, 20.0, 8),
+            (-100.2, 20.08, -20.1, 1),
+            (-100.2, 20.05, -20.0, 0),
+            (-100.0123456789, 200, -100.01234568, 8),
+        ],
+    )
+    def test_cfr(self, in_val, clamp_val, exp_val, round_val):
+        cv = geo.clamp_float_round(in_val, clamp_val, round_val)
+        assert cv == exp_val
